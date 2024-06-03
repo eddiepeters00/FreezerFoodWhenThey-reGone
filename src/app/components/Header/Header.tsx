d4ff6e8b7d3fb64ef2f7cd684f81b5dade3582c3
eddiepeters00 <eddie.peters00@outlook.com>
2024-06-03 11:48:24 +0200

    Added inner shadow to header-text

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -10,7 +10,9 @@ export default function Header() {
   return (
     <header className="m-20 text-center text-6xl h-44">
       <SocialMediaContainer />
-      <h1 className={`${permanentMarker.className} mt-10 text-purple-400 text`}>
+      <h1
+        className={`${permanentMarker.className} mt-10 text-purple-400 text text-shadow-inset`}
+      >
         Freezer Food when They&apos;re Gone
       </h1>
     </header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,33 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-};
+  plugins: [
+    //@ts-ignore
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow-inset': {
+          position: 'relative',
+          color: '#8b5cf6',
+          'background-clip': 'text',
+          'text-shadow': '4px 4px 4px rgba(0, 0, 0, 0.25)',
+          '&::before': {
+            content: '"Freezer Food when They\'re Gone"',
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            right: '0',
+            bottom: '0',
+            color: 'black',
+            'background-clip': 'text',
+            'text-shadow': 'none',
+            filter: 'blur(4px)',
+            opacity: '0.4',
+            zIndex: '1',
+          }
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],};
 export default config;
